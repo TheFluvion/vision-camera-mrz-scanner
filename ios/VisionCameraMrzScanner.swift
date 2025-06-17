08e36c843677a2075decdc424507d3b428b384c9
Gaston <96250394+TheFluvion@users.noreply.github.com>
2025-06-17 14:55:56 -0300

    config changes to V4

--- ios/VisionCameraMrzScanner.swift
+++ ios/VisionCameraMrzScanner.swift
@@ -1,141 +1,99 @@
-import Vision
-import AVFoundation
+import Foundation
+import VisionCamera
 import MLKitVision
 import MLKitTextRecognition
+import AVFoundation
 
-@objc(VisionCameraMrzScanner)
-public class VisionCameraMrzScanner: NSObject, FrameProcessorPluginBase {
+@objc(VisionCameraMrzScannerPlugin)
+public class VisionCameraMrzScannerPlugin: NSObject, FrameProcessorPlugin {
     
-    private static var textRecognizer = TextRecognizer.textRecognizer()
-    
+    private lazy var textRecognizer = TextRecognizer.textRecognizer()
+
+    public func callback(_ frame: Frame, withArguments arguments: [Any]?) -> Any? {
+        let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer)
+        guard imageBuffer != nil else {
+            print("Failed to get image buffer from frame.")
+            return nil
+        }
+
+        let visionImage = VisionImage(buffer: frame.buffer)
+        visionImage.orientation = .up
+
+        do {
+            let result = try textRecognizer.results(in: visionImage)
+            return [
+                "result": [
+                    "text": result.text,
+                    "blocks": Self.getBlockArray(result.blocks)
+                ]
+            ]
+        } catch {
+            print("Text recognition error: \(error.localizedDescription)")
+            return nil
+        }
+    }
+
+    public static func pluginName() -> String {
+        return "vision-camera-mrz-scanner"
+    }
+
     private static func getBlockArray(_ blocks: [TextBlock]) -> [[String: Any]] {
-        
-        var blockArray: [[String: Any]] = []
-        
-        for block in blocks {
-            blockArray.append([
+        return blocks.map { block in
+            [
                 "text": block.text,
                 "recognizedLanguages": getRecognizedLanguages(block.recognizedLanguages),
                 "cornerPoints": getCornerPoints(block.cornerPoints),
                 "frame": getFrame(block.frame),
                 "lines": getLineArray(block.lines),
-            ])
+            ]
         }
-        
-        return blockArray
     }
-    
+
     private static func getLineArray(_ lines: [TextLine]) -> [[String: Any]] {
-        
-        var lineArray: [[String: Any]] = []
-        
-        for line in lines {
-            lineArray.append([
+        return lines.map { line in
+            [
                 "text": line.text,
                 "recognizedLanguages": getRecognizedLanguages(line.recognizedLanguages),
                 "cornerPoints": getCornerPoints(line.cornerPoints),
                 "frame": getFrame(line.frame),
                 "elements": getElementArray(line.elements),
-            ])
+            ]
         }
-        
-        return lineArray
     }
-    
+
     private static func getElementArray(_ elements: [TextElement]) -> [[String: Any]] {
-        
-        var elementArray: [[String: Any]] = []
-        
-        for element in elements {
-            elementArray.append([
+        return elements.map { element in
+            [
                 "text": element.text,
                 "cornerPoints": getCornerPoints(element.cornerPoints),
                 "frame": getFrame(element.frame),
-            ])
-        }
-        
-        return elementArray
-    }
-    
-    private static func getRecognizedLanguages(_ languages: [TextRecognizedLanguage]) -> [String] {
-        
-        var languageArray: [String] = []
-        
-        for language in languages {
-            guard let code = language.languageCode else {
-                print("No language code exists")
-                break;
-            }
-            languageArray.append(code)
-        }
-        
-        return languageArray
-    }
-    
-    private static func getCornerPoints(_ cornerPoints: [NSValue]) -> [[String: CGFloat]] {
-        
-        var cornerPointArray: [[String: CGFloat]] = []
-        
-        for cornerPoint in cornerPoints {
-            guard let point = cornerPoint as? CGPoint else {
-                print("Failed to convert corner point to CGPoint")
-                break;
-            }
-            cornerPointArray.append([ "x": point.x, "y": point.y])
-        }
-        
-        return cornerPointArray
-    }
-    
-    private static func getFrame(_ frameRect: CGRect) -> [String: CGFloat] {
-        
-        let offsetX = (frameRect.midX - ceil(frameRect.width)) / 2.0
-        let offsetY = (frameRect.midY - ceil(frameRect.height)) / 2.0
-
-        let x = frameRect.maxX + offsetX
-        let y = frameRect.minY + offsetY
-
-        return [
-          "x": frameRect.midX + (frameRect.midX - x),
-          "y": frameRect.midY + (y - frameRect.midY),
-          "top": frameRect.maxY,
-          "left": frameRect.maxX,
-          "right": frameRect.minX,
-          "bottom": frameRect.minY,
-          "width": frameRect.width,
-          "height": frameRect.height,
-          "boundingCenterX": frameRect.midX,
-          "boundingCenterY": frameRect.midY
-        ]
-    }
-    
-    @objc
-    public static func callback(_ frame: Frame!, withArgs _: [Any]!) -> Any! {
-        
-        guard (CMSampleBufferGetImageBuffer(frame.buffer) != nil) else {
-          print("Failed to get image buffer from sample buffer.")
-          return nil
-        }
-
-        let visionImage = VisionImage(buffer: frame.buffer)
-        
-        // TODO: Get camera orientation state
-        visionImage.orientation = .up
-        
-        var result: Text
-        do {
-          result = try TextRecognizer.textRecognizer()
-            .results(in: visionImage)
-        } catch let error {
-          print("Failed to recognize text with error: \(error.localizedDescription).")
-          return nil
-        }
-        
-        return [
-            "result": [
-                "text": result.text,
-                "blocks": getBlockArray(result.blocks),
             ]
+        }
+    }
+
+    private static func getRecognizedLanguages(_ languages: [TextRecognizedLanguage]) -> [String] {
+        return languages.compactMap { $0.languageCode }
+    }
+
+    private static func getCornerPoints(_ cornerPoints: [NSValue]) -> [[String: CGFloat]] {
+        return cornerPoints.compactMap {
+            guard let point = $0 as? CGPoint else { return nil }
+            return ["x": point.x, "y": point.y]
+        }
+    }
+
+    private static func getFrame(_ frameRect: CGRect) -> [String: CGFloat] {
+        return [
+            "x": frameRect.origin.x,
+            "y": frameRect.origin.y,
+            "top": frameRect.minY,
+            "left": frameRect.minX,
+            "right": frameRect.maxX,
+            "bottom": frameRect.maxY,
+            "width": frameRect.width,
+            "height": frameRect.height,
+            "boundingCenterX": frameRect.midX,
+            "boundingCenterY": frameRect.midY
         ]
     }
 }
--- vision-camera-mrz-scanner.podspec
+++ vision-camera-mrz-scanner.podspec
@@ -11,13 +11,14 @@ Pod::Spec.new do |s|
   s.license      = package["license"]
   s.authors      = package["author"]
 
-  s.platforms    = { :ios => "11.0" }
-  s.source       = { :git => "https://github.com/mat2718/vision-camera-mrz-scanner.git", :tag => "#{s.version}" }
+  s.platforms    = { :ios => "13.4" }
+  s.source       = { :git => "https://github.com/TheFluvion/vision-camera-mrz-scanner.git", :tag => "#{s.version}" }
 
   s.source_files = "ios/**/*.{h,m,mm,swift}"
 
   s.dependency "React-Core"
   s.dependency "GoogleMLKit/TextRecognition"
+  s.dependency "VisionCamera"
 
   # Don't install the dependencies when we run `pod install` in the old architecture.
   if ENV['RCT_NEW_ARCH_ENABLED'] == '1' then
